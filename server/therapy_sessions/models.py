8706982faf494c101bc03f72a1e8c7a0bfb73289
Juan Pablo Elias <eliasjuanpablo1@gmail.com>
2022-04-07 09:58:56 -0300

    add models

--- server/therapy_sessions/models.py
+++ server/therapy_sessions/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Therapist(models.Model):
+    email = models.EmailField()
+    name = models.CharField(max_length=60)
+
+
+class Patient(models.Model):
+    email = models.EmailField()
+    name = models.CharField(max_length=60)
+
+
+class Session(models.Model):
+    fee = models.DecimalField(decimal_places=2, max_digits=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    therapist = models.ForeignKey(
+        'therapy_sessions.Therapist',
+        on_delete=models.PROTECT,
+    )
+    patient = models.ForeignKey(
+        'therapy_sessions.Patient',
+        on_delete=models.PROTECT,
+    )
+
+
+class Payment(models.Model):
+    amount = models.DecimalField(decimal_places=2, max_digits=10)
+    date = models.DateTimeField(auto_now_add=True)
+    session = models.ForeignKey(
+        'therapy_sessions.Session',
+        on_delete=models.PROTECT,
+    )
