616ddd1ddcd8b0de87d6bf6f0398a422e8d2112a
Juan Pablo Elias <eliasjuanpablo1@gmail.com>
2022-04-07 17:40:25 -0300

    add db seed migration

--- server/therapy_sessions/tests/utils.py
+++ server/therapy_sessions/tests/utils.py
@@ -2,24 +2,23 @@ from random import random
 from therapy_sessions.models import Patient, Session, Therapist
 
 
-def patient_factory() -> Patient:
-    return Patient.objects.create(
+def patient_factory(model=Patient) -> Patient:
+    return model.objects.create(
         email='test@test.com',
         name='Test Patient' + str(random()),
     )
 
 
-def therapist_factory() -> Therapist:
-    return Therapist.objects.create(
+def therapist_factory(model=Therapist) -> Therapist:
+    return model.objects.create(
         email='test@test.com',
         name='Test Therapist' + str(random()),
     )
 
 
-def session_factory(patient, therapist) -> Session:
-    return Session.create({
+def session_factory(patient, therapist, model=Session) -> Session:
+    return model.create({
         'patient': patient,
         'therapist': therapist,
         'fee': 200,
     })
-
